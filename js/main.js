f5534cf1839e4d55a0465eddb5eed94bada3a109
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-08-24 12:24:03 +0500

    Add favicon and cover

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
+// LOADER
 const elLoader = document.querySelector('.js-loader');
 
 document.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
     elLoader.classList.add('loader-wrapper--none');
-  }, 800);
+  }, 1000);
 });
+
+// OVERLAY UP
+const overlayUp = () => {
+  overlay.classList.add("overlay--hidden");
+  audioStart.play();
+  let randomNumberOne = Math.floor(Math.random() * 10) + 1,
+    randomNumberTwo = Math.floor(Math.random() * 10) + 1;
+
+  randomZone.textContent = `${randomNumberOne} * ${randomNumberTwo}`;
+  setInterval(() => {
+    randomNumberOne = Math.floor(Math.random() * 10) + 1;
+    randomNumberTwo = Math.floor(Math.random() * 10) + 1;
+    randomZone.textContent = `${randomNumberOne} * ${randomNumberTwo}`;
+    // audioFail.play();
+  }, 4000);
+};
