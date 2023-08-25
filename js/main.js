f3639af1c24fdce0dd69e0bc57669eb5844511b1
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-08-25 19:35:43 +0500

    Refactor game sound and some design

--- js/main.js
+++ js/main.js
@@ -1,82 +1,106 @@
-// LOADER
 const elLoader = document.querySelector(".js-loader");
-const elsTableData = document.querySelectorAll(".js-table-data");
-let checkClicked = false, result;
 
-document.addEventListener('DOMContentLoaded', () => {
+// Variables
+const elsTableData = document.querySelectorAll(".js-table-data");
+const ten = 10;
+const sixTeen = 16;
+const mathOper = ["/", "/", "/", "/"];
+let result;
+
+// Loader
+window.addEventListener("load", () => {
   setTimeout(() => {
     elLoader.classList.add('loader-wrapper--none');
   }, 1000);
 });
 
-// OVERLAY UP
+// Overlay up
 const gameStart = () => {
+  // Hidden overlay
   overlay.classList.add("overlay--hidden");
-  let randomNumberOne = Math.round(Math.random() * 10),
-    randomNumberTwo = Math.round(Math.random() * 10);
-  const resultStr = `${randomNumberOne} * ${randomNumberTwo}`;
+
+  // Timer
+  timer.textContent = "8s";
+  timer.classList.remove("section-game__timer--none");
+  table.classList.remove("game-table__data--pen");
+  // 1. Random numbers for table
+  let randomNumberOne = Math.round(Math.random() * ten),
+    randomNumberTwo = Math.round(Math.random() * ten);
+  // 2. Display it
+  const mathOperRandom = Math.round(Math.random() * (mathOper.length - 1));
+  if (mathOper[mathOperRandom] == "/" &&
+    randomNumberOne > randomNumberTwo &&
+    (randomNumberOne % randomNumberTwo != 0) ||
+    parseInt(randomNumberOne / randomNumberTwo) != randomNumberOne / randomNumberTwo
+  ) {
+    while (randomNumberOne % randomNumberTwo == 0 ||
+      parseInt(randomNumberOne / randomNumberTwo) != randomNumberOne / randomNumberTwo
+    ) {
+      randomNumberOne = Math.round(Math.random() * ten);
+      randomNumberTwo = Math.round(Math.random() * ten);
+    }
+  }
+  const resultStr = `${randomNumberOne} / ${randomNumberTwo}`;
   result = Function("return " + resultStr)();
   randomZone.textContent = resultStr;
-  randomNumberOne = Math.round(Math.random() * 10);
-  randomNumberTwo = Math.round(Math.random() * 10);
 
 
-  const randomIndex = Math.round(Math.random() * 16);
+  // Random index for result
+  const randomIndex = Math.round(Math.random() * sixTeen);
+
+  // Fill table with random numbers
   const randomNumbersArr = [];
-  while (!(randomNumbersArr.length == 16)) {
+  while (!(randomNumbersArr.length == sixTeen)) {
     const randomAns = Math.round(Math.random() * 100);
-    if (!(randomNumbersArr.includes(randomAns))) {
+    if (!(randomNumbersArr.includes(randomAns)) && randomAns != result) {
       randomNumbersArr.push(randomAns);
     }
   }
   elsTableData.forEach((data, index) => {
-    if (index < 4 || (index > 7 && index < 12)) {
-      if (index % 2) {
-        data.parentElement.style.backgroundColor = "#30a2ff";
-        data.parentElement.style.color = "#f7ec09";
-      } else {
-        data.parentElement.style.backgroundColor = "#f7ec09";
-        data.parentElement.style.color = "#30a2ff";
-      }
-    } else {
-      if (!(index % 2)) {
-        data.parentElement.style.backgroundColor = "#30a2ff";
-        data.parentElement.style.color = "#f7ec09";
-      } else {
-        data.parentElement.style.backgroundColor = "#f7ec09";
-        data.parentElement.style.color = "#30a2ff";
-      }
-    }
     if (randomIndex == index) {
       data.textContent = result;
     } else {
-      data.textContent = randomNumbersArr[index];
+      if (result < 0) {
+        data.textContent = -randomNumbersArr[index];
+      } else {
+        data.textContent = randomNumbersArr[index];
+      }
     }
   });
-  checkClicked = true;
 };
+
+
+
+// Start game
 startGame.addEventListener("click", () => {
   gameStart();
-  let time = 8;
+  audioStart.play();
   setInterval(() => {
-    time--;
-    console.log(time.toString().padStart(2, "0"));
-    if (!time) {
-      time = 8;
+    timer.textContent = `${timer.textContent[0] - 1}s`;
+    if (timer.textContent == "-1s") {
       gameStart();
     }
   }, 1000);
 });
+
+
+// Add click event for every element
 elsTableData.forEach(element => {
-  element.addEventListener("click", () => {
+  element.parentElement.addEventListener("click", () => {
+    table.classList.add("game-table__data--pen");
     if (element.textContent != result) {
-      audioFail.play();
-      time = 8;
-      gameStart();
+      audioWrong.play();
+      element.textContent = "😔";
+      setTimeout(() => {
+        gameStart();
+      }, 1000);
     } else {
-      audioSucces.play();
-      time = 8;
-      gameStart();
+      audioCorrect.play();
+      element.textContent = "👌";
+      setTimeout(() => {
+        gameStart();
+      }, 1000);
     }
+    timer.classList.add("section-game__timer--none");
   });
 });
